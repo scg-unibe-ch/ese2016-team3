12f73bc15454fea6b07ad43ad364b8dce9f81160
ytrehorn <astrid.ytrehorn@students.unibe.ch>
2016-10-30 15:42:09 +0100

    Made upgradeForm and -service
    need to remove upgrading-functionality from other places when it works.

---
+++ project/src/main/java/ch/unibe/ese/team3/controller/service/UpgradeService.java
@@ -0,0 +1,44 @@
+package ch.unibe.ese.team3.controller.service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import ch.unibe.ese.team3.controller.pojos.forms.UpgradeForm;
+import ch.unibe.ese.team3.model.User;
+import ch.unibe.ese.team3.model.AccountType;
+import ch.unibe.ese.team3.model.CreditcardType;
+import ch.unibe.ese.team3.model.UserRole;
+import ch.unibe.ese.team3.model.dao.UserDao;
+import ch.unibe.ese.team3.controller.service.UserService;
+
+/** Handles the upgrading of users */
+@Service
+public class UpgradeService {
+	
+	@Autowired
+	private UserDao userDao;
+	
+	@Autowired
+	private UserService userService;
+
+	/** Handles upgrading user in database. */
+	@Transactional
+	public void upgradeFrom(UpgradeForm upgradeForm) {
+		User user = userService.findUserByUsername(upgradeForm.getUsername());
+		user.setCreditCard(upgradeForm.getCreditCard());
+		user.setCreditcardType(upgradeForm.getCreditcardType());
+		user.setSecurityNumber(upgradeForm.getSecurityNumber());
+		user.setExpirationMonth(upgradeForm.getExpirationMonth());
+		user.setExpirationYear(upgradeForm.getExpirationYear());
+		user.setCreditcardName(upgradeForm.getCreditcardName());
+		
+		user.setAccountType(AccountType.PREMIUM);
+		
+		userDao.save(user);
+	}
+	
+}
