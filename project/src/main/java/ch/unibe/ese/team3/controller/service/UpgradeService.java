5c6815f8de257095a4632ba040a5b24974fd350d
Markus Eggimann <eggimannm@outlook.com>
2016-10-31 17:56:47 +0100

    Fixed some warnings. Fixed some minor html issues. Changed logger name.

--- project/src/main/java/ch/unibe/ese/team3/controller/service/UpgradeService.java
+++ project/src/main/java/ch/unibe/ese/team3/controller/service/UpgradeService.java
@@ -1,16 +1,12 @@
 package ch.unibe.ese.team3.controller.service;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import ch.unibe.ese.team3.controller.pojos.forms.UpgradeForm;
-import ch.unibe.ese.team3.model.User;
 import ch.unibe.ese.team3.model.AccountType;
-import ch.unibe.ese.team3.model.CreditcardType;
+import ch.unibe.ese.team3.model.User;
 import ch.unibe.ese.team3.model.dao.UserDao;
 
 /** Handles the upgrading of users */
