b9b649a3be07e29453c3062ce517483b7f14fad8
Markus Eggimann <eggimannm@outlook.com>
2016-11-07 19:41:22 +0100

    Added log statements in auctionservice.

--- project/src/main/java/ch/unibe/ese/team3/controller/service/AuctionService.java
+++ project/src/main/java/ch/unibe/ese/team3/controller/service/AuctionService.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import javax.transaction.Transactional;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,48 +17,47 @@ import ch.unibe.ese.team3.model.dao.BidDao;
 @Service
 public class AuctionService {
 
+	private static final Logger logger = Logger.getLogger("Ithaca logger");
+
 	@Autowired
 	private BidDao bidDao;
-	
+
 	@Autowired
 	private AdDao adDao;
-	
-	private boolean checkIfAuctionisStillRunning(Ad ad) {
-		boolean isStillRunning;
+
+	private boolean auctionIsStillRunning(Ad ad) {
 		Date endDate = ad.getEndDate();
 		Date currentDate = new Date();
 
-		if (currentDate.after(endDate))
-			isStillRunning = false;
-
-		else
-			isStillRunning = true;
-
-		return isStillRunning;
+		return !currentDate.after(endDate);
 	}
 
 	@Transactional
 	public boolean checkAndBid(Ad ad, User bidder, int amount) {
 		if (!canBid(ad, amount)) {
+			logger.info(String.format("Bid failed for ad %d by user %s", ad.getId(), bidder.getEmail()));
 			return false;
 		}
 
 		bid(ad, bidder, amount);
+		logger.info(String.format("Successful bid for ad %d by user %s, amount: %d", ad.getId(), bidder.getEmail(), amount));
 		return true;
 	}
-	
+
 	@Transactional
 	public boolean checkAndBuy(Ad ad, User purchaser) {
 		if (!canBuy(ad)) {
+			logger.info(String.format("Purchase failed for ad %d by user %s", ad.getId(), purchaser.getEmail()));
 			return false;
 		}
 
 		buy(ad, purchaser);
+		logger.info(String.format("Successful purchase for ad %d by user %s", ad.getId(), purchaser.getEmail()));
 		return true;
 	}
 
 	public void bid(Ad ad, User user, int amount) {
-		
+
 		Bid bid = new Bid();
 		bid.setAd(ad);
 		bid.setAmount(amount);
@@ -78,21 +78,21 @@ public class AuctionService {
 		ad.setcurrentAuctionPrice(ad.getcurrentAuctionPrice() + ad.getIncreaseBidPrice());
 		adDao.save(ad);
 	}
-	
+
 	private boolean canBuy(Ad ad) {
-		if (!ad.isAvailable() || !checkIfAuctionisStillRunning(ad)) {
+		if (!ad.isAvailable() || !auctionIsStillRunning(ad)) {
 			return false;
 		}
 		return true;
 	}
 
 	private boolean canBid(Ad ad, int amount) {
-		if (!ad.isAvailable() || !checkIfAuctionisStillRunning(ad)) {
+		if (!ad.isAvailable() || !auctionIsStillRunning(ad)) {
 			return false;
 		}
-		
+
 		Bid maxBid = bidDao.findTopByAdOrderByAmountDesc(ad);
-		if (maxBid != null){
+		if (maxBid != null) {
 			return maxBid.getAmount() < amount;
 		}
 		return true;
