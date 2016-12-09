5b9966aea994159fe2267c754b2fa3db395c1b27
Markus Eggimann <eggimannm@outlook.com>
2016-12-09 19:58:16 +0100

    Added test for PremiumAdComparator.

---
+++ project/src/test/java/ch/unibe/ese/team3/util/PremiumAdComparatorTest.java
@@ -0,0 +1,63 @@
+package ch.unibe.ese.team3.util;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.junit.Test;
+
+import ch.unibe.ese.team3.model.Ad;
+
+public class PremiumAdComparatorTest {
+	@Test
+	public void firstPremiumLastNotPremium(){
+		Ad first = mock(Ad.class);
+		Ad second = mock(Ad.class);
+		
+		when(first.isPremiumAd()).thenReturn(true);
+		when(second.isPremiumAd()).thenReturn(false);
+		
+		PremiumAdComparator comp = new PremiumAdComparator();
+		
+		assertEquals(-1, comp.compare(first, second));		
+	}
+	
+	@Test
+	public void firstNotPremiumLastPremium(){
+		Ad first = mock(Ad.class);
+		Ad second = mock(Ad.class);
+		
+		when(first.isPremiumAd()).thenReturn(false);
+		when(second.isPremiumAd()).thenReturn(true);
+		
+		PremiumAdComparator comp = new PremiumAdComparator();
+		
+		assertEquals(1, comp.compare(first, second));		
+	}
+	
+	@Test
+	public void bothPremium(){
+		Ad first = mock(Ad.class);
+		Ad second = mock(Ad.class);
+		
+		when(first.isPremiumAd()).thenReturn(true);
+		when(second.isPremiumAd()).thenReturn(true);
+		
+		PremiumAdComparator comp = new PremiumAdComparator();
+		
+		assertEquals(0, comp.compare(first, second));		
+	}
+	
+	@Test
+	public void bothNotPremium(){
+		Ad first = mock(Ad.class);
+		Ad second = mock(Ad.class);
+		
+		when(first.isPremiumAd()).thenReturn(false);
+		when(second.isPremiumAd()).thenReturn(false);
+		
+		PremiumAdComparator comp = new PremiumAdComparator();
+		
+		assertEquals(0, comp.compare(first, second));		
+	}
+}
